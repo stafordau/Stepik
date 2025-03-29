9b2fb2e2987c2d0b332e89a1a456b46bc841af4b
Julia <u.rascheskina@mail.ru>
2025-03-30 02:25:01 +0300

    Задача 3.6.2 изменение размера

--- src/task3_6_1/Widgets.java
+++ src/task3_6_1/Widgets.java
@@ -1,12 +1,14 @@
 package task3_6_1;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 
-class Widgets {
+public class Widgets {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Окно");
-        frame.setSize(600, 400);
+        frame.setSize(800, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new BorderLayout());
@@ -14,7 +16,22 @@ class Widgets {
         JLabel label = new JLabel("Умничка", SwingConstants.CENTER);
         label.setFont(new Font("Serif", Font.ITALIC, 50));
 
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, 5, 100, 50);
+        slider.setMajorTickSpacing(10);
+        slider.setMinorTickSpacing(5);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int fontSize = slider.getValue();
+                label.setFont(new Font("Serif", Font.ITALIC, fontSize));
+            }
+        });
+
         frame.add(label, BorderLayout.CENTER);
+        frame.add(slider, BorderLayout.SOUTH);
 
         frame.setVisible(true);
     }
