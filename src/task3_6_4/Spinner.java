2a6cdcf30b16d54fd26afc96860e37167842bba2
Julia <u.rascheskina@mail.ru>
2025-03-30 02:30:12 +0300

    Задача 3.6.5 любимый цвет

--- src/task3_6_4/Spinner.java
+++ src/task3_6_4/Spinner.java
@@ -7,37 +7,72 @@ import java.awt.event.ActionListener;
 
 class Spinner {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("JSpinner");
-        frame.setSize(300, 200);
+        JFrame frame = new JFrame("Книги и Цвет");
+        frame.setSize(400, 350);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
-        frame.add(Box.createRigidArea(new Dimension(0, 20)));
+        frame.add(Box.createRigidArea(new Dimension(0, 10)));
+        JLabel booksLabel = new JLabel("Сколько книг вы возьмете?");
+        booksLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        frame.add(booksLabel);
 
         JSpinner spinner = new JSpinner(new SpinnerNumberModel(1, 0, 100, 1));
         spinner.setMaximumSize(new Dimension(100, 30));
         spinner.setAlignmentX(Component.CENTER_ALIGNMENT);
         frame.add(spinner);
 
-        frame.add(Box.createRigidArea(new Dimension(0, 15)));
+        frame.add(Box.createRigidArea(new Dimension(0, 20)));
+        JLabel colorLabel = new JLabel("Выберите любимый цвет:");
+        colorLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        frame.add(colorLabel);
 
+        String[] colors = {"Красный", "Синий", "Зелёный", "Жёлтый"};
+        JComboBox<String> colorBox = new JComboBox<>(colors);
+        colorBox.setMaximumSize(new Dimension(200, 30));
+        colorBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+        frame.add(colorBox);
+
+        JCheckBox customCheck = new JCheckBox("Свой вариант");
+        customCheck.setAlignmentX(Component.CENTER_ALIGNMENT);
+        frame.add(customCheck);
+
+        JTextField customField = new JTextField();
+        customField.setMaximumSize(new Dimension(200, 30));
+        customField.setEnabled(false);
+        customField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        frame.add(customField);
+
+        customCheck.addActionListener(e -> customField.setEnabled(customCheck.isSelected()));
+
+        frame.add(Box.createRigidArea(new Dimension(0, 15)));
         JButton button = new JButton("Ответить");
         button.setAlignmentX(Component.CENTER_ALIGNMENT);
         frame.add(button);
 
-        frame.add(Box.createRigidArea(new Dimension(0, 15)));
-
+        frame.add(Box.createRigidArea(new Dimension(0, 10)));
         JLabel answerLabel = new JLabel("Ответ: ");
-        answerLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        answerLabel.setFont(new Font("Arial", Font.BOLD, 16));
         answerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         frame.add(answerLabel);
 
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int value = (int) spinner.getValue();
-                answerLabel.setText("Ответ: " + value);
+                int bookCount = (int) spinner.getValue();
+                String color;
+
+                if (customCheck.isSelected()) {
+                    color = customField.getText().trim();
+                    if (color.isEmpty()) {
+                        color = "(не указан)";
+                    }
+                } else {
+                    color = (String) colorBox.getSelectedItem();
+                }
+
+                answerLabel.setText("Ответ: " + bookCount + " книг, цвет: " + color);
             }
         });
 
