18b58db72daf8bc59cd6fa9e3335dcab294fa11e
Julia <u.rascheskina@mail.ru>
2025-03-31 00:15:02 +0300

    Задача 3.8.4 множественный insert

--- src/task3_8_1/insert_queries.sql
+++ src/task3_8_1/insert_queries.sql
@@ -3,3 +3,11 @@ VALUES ('Фэнтези');
 
 INSERT INTO books (title, year, author, genre_id)
 VALUES ('Властелин колец', 1954, 'Дж. Р. Р. Толкин', 3);
+
+INSERT INTO genres (genre)
+VALUES ('Роман');
+
+INSERT INTO books (title, year, author, genre_id)
+VALUES ('Мастер и Маргарита', 1967, 'Михаил Булгаков', 1),
+       ('Портрет Дориана Грея', 1891, 'Оскар Уайльд', 4),
+       ('Мартин Иден', 1909, 'Джек Лондон', 4);
